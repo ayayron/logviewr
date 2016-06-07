3e9bf1b5270a4d27ac1b82e6778e8bb898931d5c
Aaron Hoffer <aaron.hoffer@nomissolutions.com>
2016-06-06 23:35:03 -0700

    Added multilevel log filters and trycatch for missing log file

--- R/logViewrAddin.R
+++ R/logViewrAddin.R
@@ -13,7 +13,7 @@ logViewerAddin <- function() {
       selectInput("log_list", "List of Loggers", choices = list()),
       selectInput("level_filter", "At Least Level:", 
                   choices = list("DEBUG", "INFO", "WARN", "ERROR", "FATAL"),
-                  selected = "FATAL"), height = "75px"),
+                  selected = "DEBUG"), height = "75px"),
       fillRow(textInput("log_search", "Search for Log Text"), height = "50px"),
       fillRow(checkboxInput("ignore_case", "Ignore Case?", value = TRUE), height = "75px"),
       fillRow(verbatimTextOutput("log_text"))
@@ -26,30 +26,52 @@ logViewerAddin <- function() {
       # find the current objects in the R environment that are loggers
       obj_class = sapply(env, function(x){class(get(x))})
       updateSelectInput(session, "log_list", 
-                        choices = as.list(obj_class[obj_class == "logger"]))
+                        choices = names(as.list(obj_class[obj_class == "logger"])))
     })
 
     filtered_log = reactive({
       req(input$log_list)
+
+      log_levels = c("DEBUG", "INFO", "WARN", "ERROR", "FATAL")
+      if (input$level_filter == "INFO") {
+        log_levels = log_levels[2:5]
+      } else if (input$level_filter == "WARN") {
+        log_levels = log_levels[3:5]
+      } else if (input$level_filter == "ERROR") {
+        log_levels = log_levels[4:5]
+      } else if (input$level_filter == "FATAL") {
+        log_levels = log_levels[5]
+      }
+      log_levels = paste(log_levels, collapse = "|")
+
       log_file = get(input$log_list)$logfile
-      log_text = readChar(log_file, file.info(log_file)$size)
-      log_df = data.frame(lines = strsplit(log_text, "\n")[[1]],
-                          stringsAsFactors = FALSE)
-
-      # filter by the log level
-      if (length(log_df[grep(input$level_filter, log_df$lines),]) == 0) {
-        return("No logs with these filters")
-      }
-      sub_log = data.frame(lines = log_df[grep(input$level_filter, log_df$lines),],
-                           stringsAsFactors = FALSE)
-      
-      # filter by the text
-      if (length(sub_log[grep(input$log_search, sub_log$lines, ignore.case = input$ignore_case),]) == 0) {
-        return("No logs with these filters")
-      }
-
-      paste(sub_log[grep(input$log_search, sub_log$lines, 
-                         ignore.case = input$ignore_case),],collapse = "\n")
+      results = tryCatch({
+        log_text = readChar(log_file, file.info(log_file)$size)
+        
+        log_df = data.frame(lines = strsplit(log_text, "\n")[[1]],
+                            stringsAsFactors = FALSE)
+        
+        # filter by the log level
+        if (length(log_df[grep(log_levels, log_df$lines),]) == 0) {
+          return("No logs with these filters")
+        }
+        sub_log = data.frame(lines = log_df[grep(log_levels, log_df$lines),],
+                             stringsAsFactors = FALSE)
+        
+        # filter by the text
+        if (length(sub_log[grep(input$log_search, sub_log$lines, ignore.case = input$ignore_case),]) == 0) {
+          return("No logs with these filters")
+        }
+        
+        paste(sub_log[grep(input$log_search, sub_log$lines, 
+                           ignore.case = input$ignore_case),],collapse = "\n")
+      }, warning = function (w) {
+        return(w$message)
+      }, error = function (e) {
+        return(e$message)
+      }, finally = {})
+    
+      return(results)
     })
     
     output$log_text = renderText({
